f3486faa4ee28c97fe6d540f42f678f4ec16624b
Elena Kuklina <kuklina@mail.tvil.ru>
2020-02-15 18:50:15 +0400

    Initial Commit

---
+++ Shoppic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shoppic
+//
+//  Created by  Elena Kuklina on 15.02.2020.
+//  Copyright © 2020  Elena Kuklina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
